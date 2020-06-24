ae31b4aaca486aeba93aad149652f75e1930cd11
John Cabiles <johncabiles@gmail.com>
2020-06-24 12:07:52 -0700

    Add Enrolled_District and Enrolled_School columns

--- WIP_courses.sql
+++ WIP_courses.sql
@@ -1,7 +1,18 @@
 -- main view WIP
 use ccgitranscripts;
 CREATE OR REPLACE VIEW WIP_courses AS
-WITH cg AS (
+WITH ENR
+AS
+(
+select
+c.District as Enrolled_District
+, c.School as Enrolled_School
+, s.CA_STATE_STUDENT_ID
+from student s
+join cds_codes c on c.CDSCode = s.cds_code
+),
+
+cg AS (
 SELECT cg.LOCAL_COURSE_ID,
         cg.CA_STATE_STUDENT_ID,
         cg.course_grade,
@@ -14,27 +25,39 @@ SELECT cg.LOCAL_COURSE_ID,
 FROM course_grades cg
 )
 
-SELECT cds.district,
-        cds.school,
-        cg.course_grade,
-        cg.term,
-        cg.school_year,
-        cg.grade_level,
-        COUNT(DISTINCT(cg.LOCAL_COURSE_ID)) AS Total_WIP_Courses,
-        COUNT(DISTINCT(cg.CA_STATE_STUDENT_ID)) AS Total_Students_Affected,
-        cg.cds_code,
-        cg.filename
-FROM cg
+SELECT e.Enrolled_District
+        , e.Enrolled_School
+        , cds.district AS CourseTaken_District
+        , cds.school AS CourseTaken_School
+        , cg.term
+        , cg.school_year
+        , cg.grade_level
+        , COUNT(DISTINCT(cg.LOCAL_COURSE_ID)) AS Total_Number_of_Graded_Courses
+        , COUNT(DISTINCT(cg.CA_STATE_STUDENT_ID)) AS Total_Students_Affected
+        , cg.cds_code
+        , cg.filename
+FROM ENR e
+JOIN cg ON cg.CA_STATE_STUDENT_ID = e.CA_STATE_STUDENT_ID
 JOIN cds_codes cds ON cds.cdscode = cg.cds_code
 WHERE cg.work_in_progress = 'Y'
 AND cg.grade_level IN ('09', '10', '11', '12')
-GROUP BY cds.school, cg.grade_level, cg.term
-ORDER BY school_year desc, district, school, grade_level;
-
+GROUP BY e.Enrolled_District, e.Enrolled_School, cds.school, cg.grade_level, cg.term
+ORDER BY school_year desc, district, Total_Students_Affected DESC, school, grade_level;
 
 use ccgi_staging;
 CREATE OR REPLACE VIEW WIP_courses AS
-WITH cg AS (
+WITH ENR
+AS
+(
+select
+c.District as Enrolled_District
+, c.School as Enrolled_School
+, s.CA_STATE_STUDENT_ID
+from student s
+join cds_codes c on c.CDSCode = s.cds_code
+),
+
+cg AS (
 SELECT cg.LOCAL_COURSE_ID,
         cg.CA_STATE_STUDENT_ID,
         cg.course_grade,
@@ -47,28 +70,40 @@ SELECT cg.LOCAL_COURSE_ID,
 FROM course_grades cg
 )
 
-SELECT cds.district,
-        cds.school,
-        cg.course_grade,
-        cg.term,
-        cg.school_year,
-        cg.grade_level,
-        COUNT(DISTINCT(cg.LOCAL_COURSE_ID)) AS Total_WIP_Courses,
-        COUNT(DISTINCT(cg.CA_STATE_STUDENT_ID)) AS Total_Students_Affected,
-        cg.cds_code,
-        cg.filename
-FROM cg
+SELECT e.Enrolled_District
+        , e.Enrolled_School
+        , cds.district AS CourseTaken_District
+        , cds.school AS CourseTaken_School
+        , cg.term
+        , cg.school_year
+        , cg.grade_level
+        , COUNT(DISTINCT(cg.LOCAL_COURSE_ID)) AS Total_Number_of_Graded_Courses
+        , COUNT(DISTINCT(cg.CA_STATE_STUDENT_ID)) AS Total_Students_Affected
+        , cg.cds_code
+        , cg.filename
+FROM ENR e
+JOIN cg ON cg.CA_STATE_STUDENT_ID = e.CA_STATE_STUDENT_ID
 JOIN cds_codes cds ON cds.cdscode = cg.cds_code
 WHERE cg.work_in_progress = 'Y'
 AND cg.grade_level IN ('09', '10', '11', '12')
-GROUP BY cds.school, cg.grade_level, cg.term
-ORDER BY school_year desc, district, school, grade_level;
-
+GROUP BY e.Enrolled_District, e.Enrolled_School, cds.school, cg.grade_level, cg.term
+ORDER BY school_year desc, district, Total_Students_Affected DESC, school, grade_level;
 
 -- archived view total courses
 use ccgitranscripts;
 CREATE OR REPLACE VIEW WIP_courses_archived AS
-WITH cg AS (
+WITH ENR
+AS
+(
+select
+c.District as Enrolled_District
+, c.School as Enrolled_School
+, s.CA_STATE_STUDENT_ID
+from archive_student s
+join cds_codes c on c.CDSCode = s.cds_code
+),
+
+cg AS (
 SELECT cg.LOCAL_COURSE_ID,
         cg.CA_STATE_STUDENT_ID,
         cg.course_grade,
@@ -81,27 +116,39 @@ SELECT cg.LOCAL_COURSE_ID,
 FROM archive_course_grades cg
 )
 
-SELECT cds.district,
-        cds.school,
-        cg.course_grade,
-        cg.term,
-        cg.school_year,
-        cg.grade_level,
-        COUNT(DISTINCT(cg.LOCAL_COURSE_ID)) AS Total_WIP_Courses,
-        COUNT(DISTINCT(cg.CA_STATE_STUDENT_ID)) AS Total_Students_Affected,
-        cg.cds_code,
-        cg.filename
-FROM cg
+SELECT e.Enrolled_District
+        , e.Enrolled_School
+        , cds.district AS CourseTaken_District
+        , cds.school AS CourseTaken_School
+        , cg.term
+        , cg.school_year
+        , cg.grade_level
+        , COUNT(DISTINCT(cg.LOCAL_COURSE_ID)) AS Total_Number_of_Graded_Courses
+        , COUNT(DISTINCT(cg.CA_STATE_STUDENT_ID)) AS Total_Students_Affected
+        , cg.cds_code
+        , cg.filename
+FROM ENR e
+JOIN cg ON cg.CA_STATE_STUDENT_ID = e.CA_STATE_STUDENT_ID
 JOIN cds_codes cds ON cds.cdscode = cg.cds_code
 WHERE cg.work_in_progress = 'Y'
 AND cg.grade_level IN ('09', '10', '11', '12')
-GROUP BY cds.school, cg.grade_level, cg.term
-ORDER BY school_year desc, district, school, grade_level;
-
+GROUP BY e.Enrolled_District, e.Enrolled_School, cds.school, cg.grade_level, cg.term
+ORDER BY school_year desc, district, Total_Students_Affected DESC, school, grade_level;
 
 use ccgi_staging;
-CREATE OR REPLACE VIEW WIP_courses_archived AS
-WITH cg AS (
+CREATE OR REPLACE VIEW WIP_courses AS
+WITH ENR
+AS
+(
+select
+c.District as Enrolled_District
+, c.School as Enrolled_School
+, s.CA_STATE_STUDENT_ID
+from archive_student s
+join cds_codes c on c.CDSCode = s.cds_code
+),
+
+cg AS (
 SELECT cg.LOCAL_COURSE_ID,
         cg.CA_STATE_STUDENT_ID,
         cg.course_grade,
@@ -114,19 +161,21 @@ SELECT cg.LOCAL_COURSE_ID,
 FROM archive_course_grades cg
 )
 
-SELECT cds.district,
-        cds.school,
-        cg.course_grade,
-        cg.term,
-        cg.school_year,
-        cg.grade_level,
-        COUNT(DISTINCT(cg.LOCAL_COURSE_ID)) AS Total_WIP_Courses,
-        COUNT(DISTINCT(cg.CA_STATE_STUDENT_ID)) AS Total_Students_Affected,
-        cg.cds_code,
-        cg.filename
-FROM cg
+SELECT e.Enrolled_District
+        , e.Enrolled_School
+        , cds.district AS CourseTaken_District
+        , cds.school AS CourseTaken_School
+        , cg.term
+        , cg.school_year
+        , cg.grade_level
+        , COUNT(DISTINCT(cg.LOCAL_COURSE_ID)) AS Total_Number_of_Graded_Courses
+        , COUNT(DISTINCT(cg.CA_STATE_STUDENT_ID)) AS Total_Students_Affected
+        , cg.cds_code
+        , cg.filename
+FROM ENR e
+JOIN cg ON cg.CA_STATE_STUDENT_ID = e.CA_STATE_STUDENT_ID
 JOIN cds_codes cds ON cds.cdscode = cg.cds_code
 WHERE cg.work_in_progress = 'Y'
 AND cg.grade_level IN ('09', '10', '11', '12')
-GROUP BY cds.school, cg.grade_level, cg.term
-ORDER BY school_year desc, district, school, grade_level;
+GROUP BY e.Enrolled_District, e.Enrolled_School, cds.school, cg.grade_level, cg.term
+ORDER BY school_year desc, district, Total_Students_Affected DESC, school, grade_level;
